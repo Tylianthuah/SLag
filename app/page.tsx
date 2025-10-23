dbdd25d181f6db061c71fae99d794583b0d83ce7
Tylian Thuah <tylian15@gmail.com>
2025-10-23 21:53:25 +0530

    SLag Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { ThemeToggle } from '@/components/theme-toggle'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <ThemeToggle />
+    </div>
+    
+  )
+}
+
+export default Home
